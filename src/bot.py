5b883dab4da847f8cd68b8b8ab0053f0c932b89d
Tristan B. Kildaire <deavmi@localhost.localdomain>
2020-03-29 17:35:46 +0200

    d

--- src/bot.py
+++ src/bot.py
@@ -40,8 +40,6 @@ class MyClient(discord.Client):
 
     async def on_message(self, message):
 
-        if message.channel.id == 615641925466456076:
-            return
         
         print(message.reactions)
         await message.add_reaction(random.choice(emojis.troll_emojis))
