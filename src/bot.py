fd910fbfcbbbc226aafef85b90c6502c8992c036
Tristan B. Kildaire <deavmi@localhost.localdomain>
2020-03-29 14:15:21 +0200

    Added bot functionality

--- src/bot.py
+++ src/bot.py
@@ -8,12 +8,45 @@
 import discord
 from discord.ext import commands
 import os
+import subprocess
 
-bot = commands.Bot(command_prefix='>')
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+        guilds = []
+        async for guild in self.fetch_guilds():
+            print(guild)
+            guilds.append(guild)
+        c45 = guilds[0]
+        channels = await c45.fetch_channels()
+        banter = None
+        for channel in channels:
+            if channel.id==615645678147862538:
+                banter = channel
+                self.banter = banter
+                break
+       
 
-@bot.command()
-async def test(ctx, arg):
-        await ctx.send(arg)
+    async def on_message(self, message):
+        # Ignore messages from the bot
+        if message.author == self.user:
+            return
+        else:
+            # Messages from everyone else
+            if message.content.startswith(">"):
+                # Remove the `>`
+                command = message.content[1:].strip().lower()
+                print("Got command: \"" + command + "\"")
 
-token = os.getenv("C45_Token");
-bot.run(token)
+                if command == "ip:route":
+                    await self.banter.send(subprocess.getoutput("route"))
+                elif command == "ip:if":
+                    await self.banter.send(subprocess.getoutput("ifconfig"))
+                elif command == "ip:arp":
+                    await self.banter.send(subprocess.getoutput("arp"))
+            else:
+                print("Message dropped, not a command")
+            
+
+client = MyClient()
+client.run(os.getenv("C45_Token"))
