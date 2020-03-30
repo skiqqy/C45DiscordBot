265681be46e49ad17e6f96943d57cc8c39c823fe
Stephen Cochrane <skippycochrane@gmail.com>
2020-03-30 15:08:12 +0200

    Init of repo
    [Details]
    N/A
    
    [Note]
    N/A
    
    [Bugs]
    N/A
    
    [File's changed]
    	new file:   run.sh
    	new file:   src/bot.py

---
+++ src/bot.py
@@ -0,0 +1,7 @@
+##C45 Bot for the c45 discord server
+# TODO:
+# * Add functionality to access bnet
+# * Add functionality to host files
+# * Add functionality to play songs
+# * Add reminders (bot will message if something is due)
+# * Welcome messgaes and role assignment
