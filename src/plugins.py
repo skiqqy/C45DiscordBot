da97512db20e45bcc5d8b946d0c3f16d40624882
Tristan B. Kildaire <deavmi@poes.machine>
2020-04-01 18:30:21 +0200

    di

--- src/plugins.py
+++ src/plugins.py
@@ -3,7 +3,7 @@
 import importlib
 
 
-def find_plugin(config, commandString):
+def find_plugin(config, command_string):
     try:
         plugins_directory = config["plugins"]
         print("Found plugins")
@@ -12,10 +12,11 @@ def find_plugin(config, commandString):
 
         for plugin in plugins:
             print("What")
-            if plugins_directory[plugin]["command"] == commandString:
+            if plugins_directory[plugin]["command"] == command_string:
                 print("Valid plugin")
                 return importlib.import_module(plugins_directory[plugin]["file"])
         else:
             print("Invalid plugin")
     except KeyError:
         print("Couldn't find plugins key")
+
