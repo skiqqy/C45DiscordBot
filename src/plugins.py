9a6a68ba6f376363a41834b274978f3b31b801ed
Tristan B. Kildaire <deavmi@poes.machine>
2020-04-01 18:31:16 +0200

    Cleaned up

--- src/plugins.py
+++ src/plugins.py
@@ -1,5 +1,6 @@
 # Plugin loader
-
+#
+# Modular plugin loader
 import importlib
 
 def findPlugin(config, commandString):
@@ -10,9 +11,7 @@ def findPlugin(config, commandString):
 		print("Plugins available are: " + str(plugins))
 
 		for plugin in plugins:
-			print("What")
 			if pluginsDirectory[plugin]["command"] == commandString:
-				print("Valid plugin")
 				return importlib.import_module("mods."+pluginsDirectory[plugin]["file"])
 		else:
 			print("Invalid plugin")
