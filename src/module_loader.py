0a5b30b6745ccd627726a89c4b03b1d20bb308d8
Andrei Michael Dreyer <19809301@sun.ac.za>
2020-04-06 19:59:35 +0200

    :art: Removed unnecessary prints

--- src/module_loader.py
+++ src/module_loader.py
@@ -14,9 +14,7 @@ def load_modules(type_of):
     for module in modules:
         print("Module: " + str(module))
         os.chdir(type_of + "/")
-        print(module.split(".")[0])
         import importlib
-        print(eval("import pong"))
         module_imported = importlib.import_module(module.split(".")[0])
         commands[module_imported.command] = module_imported
         os.chdir("../")
